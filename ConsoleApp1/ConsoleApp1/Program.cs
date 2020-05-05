df62393cdbd2cc1e8db74c58d52700b68f494801
Matchatta Toyaem <43170683+Matchatta@users.noreply.github.com>
2020-05-05 16:59:18 +0700

    [ADD] Create my own program to convert from string to int

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regularExpression = new Regex(@"\D");
+            Console.WriteLine("Put your input or put 'exit' to terminate");
+            string input = Console.ReadLine();
+            while (input!= "exit"){
+                string result = regularExpression.Replace(input, "");
+                int intResult = StrToInt(result);
+                Console.WriteLine(intResult);
+                Console.WriteLine(intResult.GetType());
+                Console.WriteLine("Put another your input or put 'exit' to terminate");
+                input = Console.ReadLine();
+            }
+        }
+        static int StrToInt(string str)
+        {
+            int response = 0;
+            foreach (char c in str)
+            {
+                response *= 10;
+                response += c - '0';
+            }
+            return response;
+        }
+    }
+}
